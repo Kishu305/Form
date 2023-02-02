6d8bf316d2b32e93569413b0b387ea66ab9376db
Kishu.305 <kashish.madhav1930@gmail.com>
2023-02-02 16:41:50 +0530

    added src

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './header';
+import RegistrationForm from './registrationForm';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <RegistrationForm/>
+    </div>
+  );
+}
+
+export default App;
